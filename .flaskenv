44da28753d99749507dce746bbece91fbdc0d83d
blamazales <moraeslamazales@gmail.com>
2024-10-15 09:22:53 -0300

    venv changes

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
 FLASK_APP = api-exemplo.py
-FLASK_ENV = development
+
 
--- api-exemplo.py
+++ api-exemplo.py
@@ -1,3 +1,9 @@
 from app import app
+import os
+
+
+if __name__ == 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
 
 ##flask --app microblog run --debug
