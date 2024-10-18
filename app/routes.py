7fb695deefa148bb7e2e4b2fc44da13f78d307fd
blamazales <moraeslamazales@gmail.com>
2024-10-18 13:40:10 -0300

    opção de login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template
+from flask import request
+from flask import flash
+from flask import redirect
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +16,19 @@ def index(nome, idade, sexo):
 @app.route('/contato')
 def contato():
         return render_template('contato.html')
+
+
+@app.route('/login')
+def login():
+      return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if usuario == 'admin' and senha== 'senha123':
+        return "usuario: {} e senha: {}".format(usuario, senha)
+    else:
+        flash("Dados inválidos")
+        flash("Login ou senha inválidos")
+        return redirect('/login')
